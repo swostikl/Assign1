92e8fd5c500c0c17c0b40b0d6891d17882340866
Swostika lama <swostikl@metropolia.fi>
2024-09-14 13:10:19 +0300

    initial commit python module 5 second commit third commit

--- pModule5.py
+++ pModule5.py
@@ -2,31 +2,33 @@
 # and prints out the sum of the numbers. Use a for loop.
 import random
 dice=int(input("Enter the number of dices to roll: "))
+diceSet=[]
 total_sum=0
+i=0
 for i in range(dice):
     dice_roll=random.randint(1,6)
+    diceSet.append(dice_roll)
     total_sum+=dice_roll
+print(diceSet)
 print(f"The total sum of dice rolls is : {total_sum}")
 
 
 #2.Write a program that asks the user to enter numbers until they input an empty string to quit.At
 # the end, the program prints out the five greatest numbers sorted in descending order. Hint: You
 #can reverse the order of sorted list items by using the sort method with the reverse=True argument.
-#need to recheck this code
-def main():
-    numbers=[]
-    while True:
-        descend_num=int(input("Enter a number(or press enter to quit): "))
-        if descend_num=="":
-            break
-        numbers.append(descend_num)
-        if len(numbers)<=5:
-            print("not enough number, please enter number")
-        else:
-            numbers.sort(reverse=True)
-            top_five=numbers[:5]
-            print(f"The top five numbers in descending order are: {top_five}")
-main()
+
+numbers=[]
+while True:
+    num=(input("Enter a number or press enter to quit: "))
+    if num=="":
+        break
+    try:
+        number=int(num)
+        numbers.append(number)
+    except ValueError:
+        print(" Invalid number , Please enter a number")
+numbers.sort(reverse=True)
+print('The five greatest numbers are :', numbers[:5])
 
 #3.Write a program that asks the user for an integer and tells if the number is a prime number.
 # Prime numbers are number that are only divisible by one or the number itself.
@@ -34,16 +36,15 @@ main()
 #For example, 13 is a prime number as it can only be divided by 1 or 13 so that the result is an
 # integer.On the other hand, 21 is not a prime number as it is divisible by 3 and 7.
 
-#21 is not a prime number
 number=int (input("Enter a integer: "))
 if number>1:
     for i in range(2,number):
         if number%i==0:
             print(f"{number} is not a prime number")
             break
-        else:
-            print(f"{number} is a prime number")
-            break
+    else:
+        print(f"{number} is a prime number")
+
 else:
     print(f"{number} is not a prime number")
 
@@ -60,10 +61,12 @@ for i in range(5):
     city = input("Enter a name of city(or press enter to quit): ")
     if city=="":
         break
-    city_name.append(city)
+    else:
+        city_name.append(city)
+        i=i+1
 #use for/in loop to print the name of cities
 for city in city_name:
-    print(f"Here is my city name list : {city}")
+ print(city)
 
 
 
