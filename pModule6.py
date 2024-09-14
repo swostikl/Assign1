5e42a01b9a17b127f73065cc58577743b200182d
Swostika lama <swostikl@metropolia.fi>
2024-09-14 14:04:01 +0300

    initial commit python module 5 second commit third commit!

--- pModule6.py
+++ pModule6.py
@@ -18,19 +18,20 @@ roll()
 # the last exercise is that the dice rolling in the main program continues until the program gets
 # the maximum number on the dice, which is asked from the user at the beginning.
 
-def num(sides):
+import random
+def dice_roll(sides):
     return random.randint(1,sides)
 def main():
-# put user defined sides on dice
-    sides=int(input('Enter the number of the sides of dice: '))
+    sides = int(input('Enter the sides ? '))
+    attempt=0
     while True:
-        dice_roll=num(sides)
-        print(f"The rolled dice a {dice_roll}")
-        if dice_roll==sides:
-            print("You rolled the maximum number of sides")
-        break
+        rolls=dice_roll(sides)
+        attempt=attempt+1
+        print(f'The dice rolled for {attempt} and get  {rolls}')
+        if rolls==sides:
+            print(f'you rolled the maximum number! {sides}')
+            break
 main()
-
 
 
 
@@ -42,20 +43,17 @@ main()
 # converts the value to liters. The conversion must be done by using the function. Conversions
 # continue until the user inputs a negative value.
 
-def quantity():
-    while True:
-        try:
-            gasoline=float(input('Enter the quantity of gasoline(in American gallons): '))
-# 1 american gallon is 3,78541 liter
-            convert=gasoline*3.78541
-            if convert<0:
-                     print('The gas is negative, please try again')
-            else:
-                    print(f"A volume of gasoline is {convert:.2f}")
-                    break
-        except ValueError:
-            print('invalid input, please try again')
-quantity()
+def quantity_gallons(gallons):
+    litres = gallons * 3.78541
+    return litres
+while True:
+    user_input = float(input('Enter the quantity of gasoline(in American gallons): '))
+    if user_input<0:
+        break
+    else:
+        convert = quantity_gallons(user_input)
+        print(f"A volume of gasoline is {convert:.2f} in litres")
+
 
 #4.Write a function that gets a list of integers as a parameter. The function returns the sum of
 # all the numbers in the list. For testing, write a main program where you create a list, call the
@@ -80,13 +78,16 @@ def remove_odd_numbers(number):
     return[num for num in number if num % 2==0]
 #test function
 #creating list of integer
-original_list=[1,2,3,4,5,6,7,8,9,10]
+original_list=[1,1, 1,2,2,3,4,5,6,7,8,9,10]
 #call function to remove odd num
 second_list=remove_odd_numbers(original_list)
 
 #print result
 print (f"original_list: {original_list}")
 print (f"second_list: {second_list}")
+
+#0r next process
+
 
 
 #6.Write a function that receives two parameters: the diameter of a round pizza in centimeters and
